3dbfcc7cf38b6bf800e4385f17220cf34b8d7019
johnrott <jrott326@gmail.com>
2025-11-10 18:44:39 -0700

    Update datapath3by3accelerator.v
    updated comments

--- datapath3by3accelerator.v
+++ datapath3by3accelerator.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/30/2025 10:19:10 PM
-// Design Name: 
-// Module Name: datapath3by3accelerator
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module datapath3by3accelerator(
