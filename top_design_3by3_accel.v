7114653d60563530cc4c4f8d06490ad8c276c2bb
johnrott <jrott326@gmail.com>
2025-11-10 18:45:52 -0700

    Update top_design_3by3_accel.v
    added in UART transmissions

--- top_design_3by3_accel.v
+++ top_design_3by3_accel.v
@@ -1,46 +1,24 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/01/2025 11:59:12 AM
-// Design Name: 
-// Module Name: top_design_3by3_accel
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module top_design_3by3_accel(
-    output led0_g, led0_r,
-    output [4:0] ja,
+    output led0_g,
+    output [3:0] ja,
     input [1:0] btn,
     input clk
     );
     
-    // ---------- MATRIX INPUTS ----------
+
     wire [7:0] a11, a12, a13, a21, a22, a23, a31, a32, a33;
     wire [7:0] x1, x2, x3;
     wire [15:0] y1, y2, y3;
     
-    // ---------- CONTROL & OUTPUT ----------
-    reg [3:0] tempOut;
-    wire ClkOut;
-    reg [3:0] count;
-    reg resetFlag;
+   
     wire doneFlag;
-    reg [3:0] pulseCount; // used to stretch the done pulse
+    assign ja[3] = btn[0];
+
     
-    // ---------- CONSTANT MATRIX VALUES ----------
+   
     assign x1 = 1;
     assign x2 = 1;
     assign x3 = 1;
@@ -57,11 +35,9 @@ module top_design_3by3_accel(
     assign a32 = 1;
     assign a33 = 2;
     
-    // ---------- OUTPUT CONNECTIONS ----------
-    assign ja[3:0] = tempOut;
-    assign ja[4] = resetFlag;
+
     
-    // ---------- MODULE INSTANTIATIONS ----------
+
     datapath3by3accelerator dp(
         .clk(clk),
         .reset(btn[0]),
@@ -73,55 +49,42 @@ module top_design_3by3_accel(
         .y1(y1), .y2(y2), .y3(y3),
         .done(doneFlag)
     );
-        
-    ClkDiv cd(clk, 1'b0, ClkOut);
     
-    assign led0_g = ja[4];
-    assign led0_r = ja[0];
-    
-    // ---------- MAIN OUTPUT SEQUENCER ----------
-    always @(posedge ClkOut or posedge btn[0]) begin
-        if (btn[0]) begin
-            count <= 0;
-            tempOut <= 0;
-            resetFlag <= 0;
-            pulseCount <= 0;
+    reg done_prev;
+    always @(posedge clk) begin
+        if (btn[0]) begin // reset
+            done_prev <= 0;
         end
         else begin
-            // Hold resetFlag HIGH for a few cycles for Arduino to catch
-            if (pulseCount > 0) begin
-                pulseCount <= pulseCount - 1;
-                if (pulseCount == 1)
-                    resetFlag <= 0;
-            end
-            else if (doneFlag) begin
-                case (count)
-                    4'd0: begin
-                        tempOut <= y1[3:0];
-                        count <= 4'd1;
-                        resetFlag <= 1;
-                        pulseCount <= 4'd3;
-                    end
-                    4'd1: begin
-                        tempOut <= y2[3:0];
-                        count <= 4'd2;
-                        resetFlag <= 1;
-                        pulseCount <= 4'd3;
-                    end
-                    4'd2: begin
-                        tempOut <= y3[3:0];
-                        count <= 4'd0; // wrap around
-                        resetFlag <= 1;
-                        pulseCount <= 4'd3;
-                    end
-                    default: begin
-                        count <= 0;
-                        resetFlag <= 0;
-                    end
-                endcase
-            end
+            done_prev <= doneFlag;
         end
     end
+
+wire transmitPulse = doneFlag & ~done_prev;
+     assign led0_g = transmitPulse;
+    
+
+    transmitter y1output(
+        .data(y1[7:0]),
+        .clk(clk),
+        .reset(btn[0]),
+        .transmit(transmitPulse),
+        .TxD(ja[0]));
+    
+    transmitter y2output(
+        .data(y2[7:0]),
+        .clk(clk),
+        .reset(btn[0]),
+        .transmit(transmitPulse),
+        .TxD(ja[1]));
+    
+    transmitter y3output(
+        .data(y3[7:0]),
+        .clk(clk),
+        .reset(btn[0]),
+        .transmit(transmitPulse),
+        .TxD(ja[2]));
+   
     
 endmodule
     
